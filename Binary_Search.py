967c4dc8b8dc1dc2c0b21a22a3b4755f3ccf3500
Himanshu Ravne <122271334+Himanshu-565@users.noreply.github.com>
2023-05-26 08:58:44 +0530

    Update Binary_Search.py

--- Binary_Search.py
+++ Binary_Search.py
@@ -1,28 +1,19 @@
+n = int(input("Enter the size of the array: "))
+arr = list(map(int, input("Enter the array elements: ").split()))
+target = int(input("Enter the target element: "))
+start = 0
+end = n-1
+mid = (start+end)//2
 
-
-def binary_search(arr, target):
-    low = 0
-    high = len(arr) - 1
-
-    while low <= high:
-        mid = (low + high) // 2
-
-        if arr[mid] == target:
-            return mid
-        elif arr[mid] < target:
-            low = mid + 1
-        else:
-            high = mid - 1
-
-    return -1  # Target element not found
-
-# Example usage:
-array = [2, 5, 8, 12, 16, 23, 38, 56, 72, 91]
-target = 23
-
-index = binary_search(array, target)
-
-if index != -1:
-    print("Target element {target} found at index {index}.")
+while(start <= end):
+    mid = (start+end)//2
+    if(arr[mid] == target):
+        print("Element found at index ", mid)
+        break
+    elif(arr[mid] > target):
+        end = mid - 1
+    else:
+        start = mid + 1
+    
 else:
-    print("Target element not found in the array.")
+    print("Element not found")
